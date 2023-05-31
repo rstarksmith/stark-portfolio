2b73737f825ce7ec4c553fd007bcbc97b862d9c7
Rachel <rstark.smith@gmail.com>
2023-05-30 19:59:34 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Navbar from "./scenes/Navbar"
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home')
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px")
+
   return (
-    <div>
+    <div className="app bg-deep-blue">
+      <Navbar
+        selectedPage ={selectedPage}
+        setSelectedPage={setSelectedPage}
+        />
     welcome
     </div>
   );
