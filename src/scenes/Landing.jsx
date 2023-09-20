bb5132d1d75721e46e4857287efd2eb9c2d55f29
Rachel <rstark.smith@gmail.com>
2023-09-20 16:30:37 -0500

    landing - remove img border

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -10,11 +10,13 @@ const Landing = ({setSelectedPage}) => {
         <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
             {/* Image Section */}
             
-            <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+            <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-12 md:mt-12">
                 {isAboveMediumScreens ? (
-                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-right-20 
-                        before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
-                        before:border-2 before:border-blue before:z-[-1]"
+                    <div 
+                    //image line backdrop
+                    // className="relative  before:absolute before:-top-10 before:-right-20 
+                    //     before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
+                    //     before:border-2 before:border-blue before:z-[-1]"
                     >
                         <img alt="profile"
                             className="hover:filter hover:grayscale transition duration-500 z-10 w-full
