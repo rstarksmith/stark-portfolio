a587b2bd1d7b14f6e91ea91884b34bb52da0df84
Rachel <rstark.smith@gmail.com>
2023-09-20 16:46:00 -0500

    landing - add insta icon, socialmediaicons - setup insta

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -25,14 +25,14 @@ const SocialMediaIcons = () => {
       >
         <img alt="github-link" src="../assets/github.png" />
       </a>
-      {/* <a
+      <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com"
+        href="https://www.instagram.com/rstark.smith/"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="instagram-link" src="../assets/instagram.png" />
-      </a> */}
+      </a>
     </div>
   );
 };
