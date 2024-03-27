f7735558e53a11d569ba05efe6a914c8f3b158e8
Rachel <rstark.smith@gmail.com>
2024-03-27 16:11:34 -0500

    update resume

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage}) => {
     return (
         <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
-                <h4 className="font-playfair text-3xl font-bold">♡</h4>
+                <h4 className="font-playfair text-3xl font-bold"></h4>
 
                 {/* DESKTOP NAV */}
                 {isAboveSmallScreens ? (<div className="flex justify-between tracking-wide gap-16 font-opensans text-sm font-semibold">
